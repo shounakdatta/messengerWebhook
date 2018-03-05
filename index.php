5a4914e5de3c868537a4e24937f0acad0e96f469
shounakdatta <33079683+shounakdatta@users.noreply.github.com>
2018-03-05 10:47:00 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if($method == "POST") {
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
     
-    $requestName = file_get_contents("https://graph.facebook.com/v2.6/<PSID>?fields=first_name,last_name,profile_pic&access_token=<EAADBZCCEtqIwBALT26azIaBKPZBQoeVdXCPURAn95XIgKTdez45dL27BGZApTNjUGgTJZB6JpeQcdTHD84w7QZCygRyzvmHVisZATWZAI2zxhyl7ZCSYF2mBXimKIoAuq7p9nyCrZAQFpvracMiwclyoNxALHSiXRZBi5S6pKAOoNj2zcbeZBtQdDrd>"
+    $requestName = file_get_contents("https://graph.facebook.com/v2.6/<PSID>?fields=first_name,last_name,profile_pic&access_token=<PAGE_ACCESS_TOKEN>";
     $name = json_decode($requestName);
 
     $text = $json->result->parameters->text;
